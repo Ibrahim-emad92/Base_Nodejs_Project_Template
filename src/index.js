f2aabafc9f2121ef0d3218c104e5ba99398dad26
Ibrahim-emad92 <ibrahimemad953@yahoo.com>
2026-06-11 18:23:56 +0300

    Add basic node express functionalitities

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const {ServerConfig} = require('./config');
+const express = require('express');
+const apiRoutes = require('./routes');
+const app = express();
+
+app.use('/api',apiRoutes);
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Server is running on port ${ServerConfig.PORT}`);
+});
